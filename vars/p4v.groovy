92b9e3f596815938173239e56cfb000a1c96993e
Feiko van Dijk <feiko60@gmail.com>
2021-04-10 00:41:18 +0200

    Update p4v.groovy

--- vars/p4v.groovy
+++ vars/p4v.groovy
@@ -10,7 +10,7 @@ def init(p4credential, p4host, p4workspace, p4viewMapping, cleanForce = true)
    }
    else
    {
-      p4sync charset: 'none', credential: p4Info.credential, format: 'jenkins-${JOB_NAME}', populate: syncOnly(delete: false, modtime: false, parallel: [enable: true, minbytes: '1024', minfiles: '1', threads: '4'], pin: '', quiet: true), source: templateSource(p4Info.workspace)
+      p4sync charset: 'none', credential: p4Info.credential, format: 'jenkins-${JOB_NAME}', populate: syncOnly(delete: false, modtime: false, parallel: [enable: true, minbytes: '1024', minfiles: '1', threads: '4'], pin: '', quiet: true, replace: true, tidy: false), source: templateSource(p4Info.workspace)
    }
 }
 
