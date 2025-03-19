a079ac2a1e72fe21c5c2b32f212b20e67733145c
Feiko van Dijk <feiko60@gmail.com>
2025-03-19 20:48:52 +0100

    Added P4 Streams functionality

--- vars/p4v.groovy
+++ vars/p4v.groovy
@@ -1,12 +1,16 @@
 def p4Info = null
 
 // Must be called first before calling other functions
-def init(p4credential, p4host, p4workspace, p4viewMapping, cleanForce = true)
+def init(p4credential, p4host, p4workspace, p4viewMapping, cleanForce = true, streamDepot = false)
 {
    p4Info = [credential: p4credential, host: p4host, workspace: p4workspace, viewMapping: p4viewMapping]
    if (cleanForce)
    {
       p4sync charset: 'none', credential: p4Info.credential, format: 'jenkins-${JOB_NAME}', populate: forceClean(have: false, parallel: [enable: true, minbytes: '1024', minfiles: '1', threads: '4'], pin: '', quiet: true), source: templateSource(p4Info.workspace)
+   }
+   if (streamDepot)
+   {
+      p4sync charset: 'none', credential: 'p4credential', populate: autoClean(delete: true, modtime: false, parallel: [enable: false, minbytes: '1024', minfiles: '1', threads: '4'], pin: '', quiet: true, replace: true, tidy: false), source: streamSource('p4Info.workspace')
    }
    else
    {
